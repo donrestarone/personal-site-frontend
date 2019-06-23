366ecc7a798cb2ff040553bcc992c39f36d63901
donrestarone <shashikejayatunge@gmail.com>
2019-06-23 10:09:52 -0400

    remove bootstrap rip

--- shashike/src/index.js
+++ shashike/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // bootstrap
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 require('dotenv').config()
 
