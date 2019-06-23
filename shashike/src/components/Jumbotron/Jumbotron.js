73dec19f91b43cf8dd629b34129fd621999ef183
donrestarone <shashikejayatunge@gmail.com>
2019-06-23 10:08:54 -0400

    remove bootstrap

--- shashike/src/components/Jumbotron/Jumbotron.js
+++ shashike/src/components/Jumbotron/Jumbotron.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import './Jumbotron.css'
 
 const Jumbotron = (props) => {
-  console.log(props.exit)
   return (
-    <div class={`jumbotron jumbotron-fluid animated ${props.exit}`}>
-      <div class="container">
-        <h1 class="display-2">I am</h1>
-        <p className={`display-4 animated ${props.style}`}>{props.word}</p>
+    <div className={`jumbo-wrapper ${props.exit}`}>
+      <div>
+        <h1 className="jumbo-heading">I am</h1>
+        <p className={`jumbo-text ${props.style}`}>{props.word}</p>
       </div>
     </div>
   )
