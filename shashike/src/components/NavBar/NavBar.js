4daf6cebd3c9f80404460e2fdc7df825026ae87c
donrestarone <shashikejayatunge@gmail.com>
2019-08-26 15:12:21 -0400

    revamp navbar

--- shashike/src/components/NavBar/NavBar.js
+++ shashike/src/components/NavBar/NavBar.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import './NavBar.css'
 class NavBar extends Component {
-
   render() {
     return (
-      <div className="navbar-wrapper">
-        <div className="navbar-header">
-          
-        </div>
-        <div className="navbar-body">
-          
-        </div>
+      <div className="navbar">
+        <h1 className="navbar-title">Shashike J</h1>
+        <button className="contact-button">Contact</button>
       </div>
     );
   }
