7da3792a3dc4fb8991475e6981ee8dfdf07aadf1
donrestarone <shashikejayatunge@gmail.com>
2019-08-27 11:44:41 -0400

    convert to class method

--- shashike/src/components/Containers/Welcome/Welcome.js
+++ shashike/src/components/Containers/Welcome/Welcome.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './welcome.css'
+import {Link} from 'react-router-dom'
 
-const Welcome = (props) => {
-  return (
-    <div className="project-type-wrapper">
-      <div className="professional-projects-wrapper">
-        <h1>Professional Projects</h1>
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="project-type-wrapper" ref={welcomeContainer => {this.welcomeContainer = welcomeContainer}} >
+        <div className="professional-projects-wrapper">
+          <Link to="/professional-projects" className="projects-gateway">Professional Projects</Link>
+        </div>
+        <div className="divider"></div>
+        <div className="personal-projects-wrapper">
+          <Link to="/personal-projects" className="projects-gateway">Personal Projects</Link> 
+        </div>
       </div>
-      <div className="divider"></div>
-      <div className="personal-projects-wrapper">
-        <h1>Personal Projects</h1> 
-      </div>
-    </div>
-  )
+    );
+  }
 }
 
 export default Welcome;
