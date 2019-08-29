9e85701645a18f9c7815bd724382f5d8a7a1c0e6
donrestarone <shashikejayatunge@gmail.com>
2019-08-29 16:54:55 -0400

    add personal project

--- shashike/src/Constants/PersonalProjectsData.js
+++ shashike/src/Constants/PersonalProjectsData.js
@@ -7,4 +7,12 @@ export const personalProjects = [
     heroAssetType: 'video',
     description: 'This was a coding challenge I completed for a full stack web developer position at Fintros in 2018. I used a rails 5 API backend with a vanilla javascript front end.'
   },
+  {
+    title: 'Menrva: A Simple Textual Analysis Tool',
+    projectLink: 'https://menrva.herokuapp.com/',
+    githubLink: 'https://github.com/donrestarone/menrva',
+    heroAssetLink: 'https://shashike-portfolio-site-assets.s3.amazonaws.com/hacker-news-clone.mov',
+    heroAssetType: 'video',
+    description: 'When given a large paragraph of text, Menrva will return some word statistics.'
+  },
 ]
