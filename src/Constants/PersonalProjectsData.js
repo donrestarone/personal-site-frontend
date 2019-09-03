4c60046a6921410390e143ddcbe9ff62df3dfafb
donrestarone <shashikejayatunge@gmail.com>
2019-09-03 12:12:00 -0400

    add railroad and warning to modal

--- src/Constants/PersonalProjectsData.js
+++ src/Constants/PersonalProjectsData.js
@@ -15,4 +15,12 @@ export const personalProjects = [
     heroAssetType: 'video',
     description: 'When given a large paragraph of text, Menrva will return some word statistics.'
   },
-]
+  {
+    title: 'RailRoad: Your Secret Messenger',
+    projectLink: 'https://rail-room.herokuapp.com/',
+    githubLink: 'https://github.com/donrestarone/rail-room',
+    heroAssetLink: 'https://shashike-portfolio-site-assets.s3.amazonaws.com/hacker-news-clone.mov',
+    heroAssetType: 'video',
+    description: 'RailRoom is a privacy oriented real time messaging app built with React, Redux and Rails 5 ActionCable. Only users who are given the room link are able to connect, view and send messages.'
+  },
+].reverse()
