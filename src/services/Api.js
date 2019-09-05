7ac10c57b85b35d52996fcdb397db22960224b3f
donrestarone <shashikejayatunge@gmail.com>
2019-09-05 16:30:19 -0400

    point to prod

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,5 +2,5 @@ const localRoot = 'http://localhost:4000'
 const productionRoot = 'https://shashike-j.herokuapp.com'
 
 export const getApiRoot = () => {
-  return localRoot
+  return productionRoot
 }
