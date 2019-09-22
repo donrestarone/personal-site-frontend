e8a4f57ea7e485359cc4a40fd2e065cc50c515ff
donrestarone <shashikejayatunge@gmail.com>
2019-09-22 15:21:29 -0400

    add paragraoh

--- src/components/Containers/AboutMe/AboutMe.js
+++ src/components/Containers/AboutMe/AboutMe.js
@@ -6,6 +6,7 @@ const AboutMe = (props) => {
       <div className="about-me-wrapper">
         <strong>Hi &#9996;</strong>
         <p>I'm a full stack web developer based in Toronto who loves solving business problems with various web technologies.</p>
+        <p>I enjoy writing code that matters &amp; building minimum-viable-products to realize new ideas.</p>
         <p>When I'm not coding, you'll find me either at the gym or working on my car.</p>
       </div>
     </>
