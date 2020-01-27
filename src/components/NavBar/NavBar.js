15a2467fa4cf1f9d1d31ef05d7ae09e4e67af016
donrestarone <shashikejayatunge@gmail.com>
2020-01-27 09:24:19 -0500

    add animation

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,13 +4,13 @@ import './NavBar.css'
 class NavBar extends Component {
   render() {
     return (
-      <div className="navbar">
+      <div className="navbar animated slideInDown">
         <div className="navbar-home">
           <Link className="navbar-title" to="/" onClick={this.props.closeContactModal}>Shashike J</Link>
         </div>
           <div className="nav-buttons-wrapper">
             <Link to="/about/me" className="nav-button" onClick={this.props.closeContactModal}>About</Link>
-            <Link to="/projects" className="nav-button" onClick={this.props.closeContactModal}>App Showcase</Link>
+            <Link to="/projects" className="nav-button" onClick={this.props.closeContactModal}>Projects</Link>
             <Link to="/contact" className="nav-button" onClick={this.props.closeContactModal}>Contact</Link>
           </div>
       </div>
