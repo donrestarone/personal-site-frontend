faecbcd5534c1ab29dfaea48d76c3f7fca3c81b7
donrestarone <shashikejayatunge@gmail.com>
2020-01-27 11:08:25 -0500

    new markup

--- src/components/Containers/AboutMe/AboutMe.js
+++ src/components/Containers/AboutMe/AboutMe.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './AboutMe.css'
+import ProfileImageDesktop from '../../../assets/profile-image-desktop.png'
 const AboutMe = (props) => {
   return (
-    <>
+    <div className="about-me-content-wrapper">
       <div className="about-me-wrapper">
-        <strong>Hi &#9996;</strong>
-        <p>I'm a full stack web developer based in Toronto who loves solving business problems with various web technologies.</p>
-        <p>I enjoy writing code that matters &amp; building minimum-viable-products to realize new ideas.</p>
-        <p>When I'm not coding, you'll find me either at the gym or working on my car.</p>
+        <div className="about-me-text-wrapper">
+          <h1 className="about-me-heading">About me</h1>
+          <p className="about-me-subheading">I am a designer, developer and dev ops engineer from Toronto </p>
+        </div>
+        <div className="about-me-image-wrapper">
+          <img src={ProfileImageDesktop}></img>
+        </div>
+        <div className="about-me-extended-wrapper">
+          <p className="about-me-extended-wrapper-subheading">My track record consists mainly of building MVP’s and SaaS products.</p>
+          <p className="about-me-extended-wrapper-subheading">I love solving business problems with various web technologies &amp; enjoy writing code that matters.</p>
+          <p className="about-me-extended-wrapper-subheading">When I’m not building software, you'll find me either at the gym, cooking or playing guitar.</p>
+        </div>
       </div>
-    </>
+        <div className="about-me-social-media-wrapper">
+          <a className="about-me-link" target="_blank" href="https://github.com">Github</a>
+          <a className="about-me-link" target="_blank" href="https://github.com">shashikejayatunge@gmail.com</a>
+          <a className="about-me-link" target="_blank" href="https://github.com">LinkedIn</a>
+        </div>
+    </div>
   )
 }
 
