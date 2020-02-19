6f53a253d809c7147c68180f72fb3047c995f114
donrestarone <shashikejayatunge@gmail.com>
2020-02-18 20:48:05 -0500

    refactor styles into media queries

--- src/components/Project/VideoContainer/VideoContainer.js
+++ src/components/Project/VideoContainer/VideoContainer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class VideoContainer extends Component {
   render() {
     return (
-      <video className={this.props.media.className} src={this.props.media.link} controls />
+      <video className={this.props.media.className} src={this.props.media.link} autoPlay muted="" playsinline="" controls loop />
     );
   }
 }
